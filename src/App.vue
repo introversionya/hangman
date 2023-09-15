cf363c70567cbfabf26b11a3788c7dc10b4cd4c6
Anton Demidenko <yaintroversivnost@gmail.com>
2023-09-15 13:54:07 +0500

    Логика сброса игры(restart)

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,26 @@ import GameWrongLetters from './components/GameWrongLetters.vue'
 import GameWord from './components/GameWord.vue'
 import GamePopup from './components/GamePopup.vue'
 import GameNotification from './components/GameNotification.vue'
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 
 const word = ref('василий')
 const letters = ref<string[]>([])
 const correctLetters = computed(() => letters.value.filter((x) => word.value.includes(x)))
 const wrongLetters = computed(() => letters.value.filter((x) => !word.value.includes(x)))
 const notification = ref<InstanceType<typeof GameNotification> | null>(null)
+const popup = ref<InstanceType<typeof GamePopup> | null>(null)
+
+watch(wrongLetters, () => {
+  if (wrongLetters.value.length === 6) {
+    popup.value?.open('lose')
+  }
+})
+
+watch(correctLetters, () => {
+  if (correctLetters.value.length === word.value.length) {
+    popup.value?.open('win')
+  }
+})
 
 window.addEventListener('keydown', ({ key }) => {
   if (letters.value.includes(key)) {
@@ -24,6 +37,11 @@ window.addEventListener('keydown', ({ key }) => {
     letters.value.push(key.toLocaleLowerCase())
   }
 })
+
+const restart = () => {
+  letters.value = []
+  popup.value?.close()
+}
 </script>
 
 <template>
@@ -35,6 +53,6 @@ window.addEventListener('keydown', ({ key }) => {
     <GameWord :word="word" :correctLetters="correctLetters" />
   </div>
 
-  <GamePopup v-if="false" />
+  <GamePopup ref="popup" :word="word" @restart="restart" />
   <GameNotification ref="notification" />
 </template>
